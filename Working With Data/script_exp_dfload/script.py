f16e0f466c5467f25a077e3ea35ec364bdfe79b0
SureshGorakala <sureshkumargorakala@gmail.com>
2022-03-09 19:36:37 +0530

    new changes

---
+++ Working With Data/script_exp_dfload/script.py
@@ -0,0 +1,13 @@
+from azureml.core import Run,Dataset
+import pandas as pd
+import argparse as arg
+
+parser = arg.ArgumentParser()
+parser.add_argument('--ds',type=str,dest='dataset_name')
+args = parser.parse_args()
+
+
+run = Run.get_context()
+ws = run.experiment.workspace
+dataset = Dataset.get_by_name(ws,name=args.dataset_name)
+dataset.take(3).to_pandas_dataframe()
